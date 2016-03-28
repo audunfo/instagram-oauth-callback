c3e73d0544b855ed2311bcca28a820057dd7b5eb
Audun Follegg <audunfo@gmail.com>
2016-03-28 14:49:19 +0200

    Add openshift port and ip

--- server.js
+++ server.js
@@ -8,19 +8,22 @@ var express = require('express');
 var url = require('url');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 app.get('/oauth/callback', function(req, res){
 	res.sendStatus(200);
 });
 
 app.get('/', function(req, res){
-	res.send('Simple callback API ');
+	res.send('Simple API backend for instaSwipe');
+		
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), app.get('ip'));
+/*app.listen(app.get('port'), function() {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
-});
+});*/
 
 module.exports = app;
 
